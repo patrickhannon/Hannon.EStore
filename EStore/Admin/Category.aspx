d2aa9914c4c9bed569642cdc817e933fc658ab17
Patrick Hannon <patrick_hannon@hotmail.com>
2018-09-27 08:36:58 -0500

    added new fields to admin page

---
+++ EStore/Admin/Category.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Category.aspx.cs" Inherits="EStore.Admin.Category" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Add Category:</h3>
+    <table>
+        <tr>
+            <td><asp:Label ID="LblAddCategory" runat="server">Name:</asp:Label></td>
+            <td>
+                <asp:TextBox ID="AddCategory" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator9" runat="server" Text="* Category name required." ControlToValidate="AddCategory" SetFocusOnError="true" Display="Dynamic"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="LblAddCategoryDesc" runat="server">Description:</asp:Label></td>
+            <td>
+                <asp:TextBox ID="AddCategoryDesc" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator10" runat="server" Text="* Description required." ControlToValidate="AddCategoryDesc" SetFocusOnError="true" Display="Dynamic"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+    </table>
+    <p></p>
+    <p></p>
+    <asp:Button ID="AddCategoryButton" runat="server" Text="Add Category" OnClick="AddCategoryButton_Click"  CausesValidation="true"/>
+    <asp:Label ID="LabelAddStatus" runat="server" Text=""></asp:Label>
+    <p></p>
+    <h3>Remove Category:</h3>
+    <table>
+        <tr>
+            <td><asp:Label ID="LabelRemoveCategory" runat="server">Product:</asp:Label></td>
+            <td><asp:DropDownList ID="DropDownRemoveCategory" runat="server" ItemType="EStore.Models.Category" 
+                                  SelectMethod="GetCategories" AppendDataBoundItems="true" 
+                                  DataTextField="CategoryName" DataValueField="CategoryId" >
+                </asp:DropDownList>
+            </td>
+        </tr>
+    </table>
+    <p></p>
+    <asp:Button ID="RemoveCategoryButton" runat="server" Text="Remove Category" OnClick="RemoveCategoryButton_Click" CausesValidation="false"/>
+    <asp:Label ID="LabelRemoveStatus" runat="server" Text=""></asp:Label>
+</asp:Content>
---
+++ EStore/Admin/Category.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EStore.Logic;
+
+namespace EStore.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string categoryAction = Request.QueryString["CategoryAction"];
+            if (categoryAction == "add")
+            {
+                LabelAddStatus.Text = "Category added!";
+            }
+
+            if (categoryAction == "remove")
+            {
+                LabelRemoveStatus.Text = "Category removed!";
+            }
+        }
+
+        protected void AddCategoryButton_Click(object sender, EventArgs e)
+        {
+            AddCategories categories = new AddCategories();
+            bool addSuccess = categories.AddCategory(AddCategory.Text, AddCategoryDesc.Text);
+            if (addSuccess)
+            {
+                // Reload the page.
+                string pageUrl = Request.Url.AbsoluteUri.Substring(0, Request.Url.AbsoluteUri.Count() - Request.Url.Query.Count());
+                Response.Redirect(pageUrl + "?CategoryAction=add");
+            }
+            else
+            {
+                LabelAddStatus.Text = "Unable to add new category to database.";
+            }
+        }
+        protected void RemoveCategoryButton_Click(object sender, EventArgs e)
+        {
+            using (var _db = new EStore.Models.ProductContext())
+            {
+                int categoryId = Convert.ToInt16(DropDownRemoveCategory.SelectedValue);
+                var myItem = (from c in _db.Categories where c.CategoryId == categoryId select c).FirstOrDefault();
+                if (myItem != null)
+                {
+                    _db.Categories.Remove(myItem);
+                    _db.SaveChanges();
+
+                    // Reload the page.
+                    string pageUrl = Request.Url.AbsoluteUri.Substring(0, Request.Url.AbsoluteUri.Count() - Request.Url.Query.Count());
+                    Response.Redirect(pageUrl + "?CategoryAction=remove");
+                }
+                else
+                {
+                    LabelRemoveStatus.Text = "Unable to locate category.";
+                }
+            }
+        }
+        public IQueryable GetCategories()
+        {
+            var _db = new EStore.Models.ProductContext();
+            IQueryable query = _db.Categories;
+            return query;
+        }
+    }
+}
